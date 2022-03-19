fe74c50d88a2e5026e033ad3ea2edba65a617e3e
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2022-03-19 11:28:51 +0600

    Create helicaptor.java

---
+++ helicaptor.java
@@ -0,0 +1,55 @@
+
+public class helicaptor {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		while (true) {
+
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+
+			System.out.println("         :LOL:Awais:Awais");
+			System.out.println("           A           ");
+			System.out.println("       /--------       ");
+			System.out.println(" LOL====      ()\\     ");
+			System.out.println("  L    \\         \\        ");
+			System.out.println("        \\_________]                ");
+			System.out.println("           I     I      ");
+			System.out.println("        -----------/              ");
+			System.out.println("                       ");
+			System.out.println("      Awais Helicopter!              ");
+
+			Thread.sleep(100);
+
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+
+			System.out.println("Awais:Awais:LOL:         ");
+			System.out.println("           A           ");
+			System.out.println("  L    /--------       ");
+			System.out.println(" LOL====      ()\\     ");
+			System.out.println("       \\         \\        ");
+			System.out.println("        \\_________]                ");
+			System.out.println("           I     I      ");
+			System.out.println("        -----------/              ");
+			System.out.println("                       ");
+			System.out.println("      Awais Helicopter!              ");
+
+			Thread.sleep(100);
+		}
+
+	}
+
+}
