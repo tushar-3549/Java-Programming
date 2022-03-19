273b3436ea44c48d519685ea5938cd5ba04e3c25
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2022-03-19 11:39:51 +0600

    Create Main.java

---
+++ Employee/Main.java
@@ -0,0 +1,13 @@
+package Employee;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Owner o = new Owner(20200,465, 2, 1);
+		System.out.println("Total:"+o.getSalary());
+
+	}
+
+	
+
+}
